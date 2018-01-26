ee7d5274dda4e8ecda584f64a57b50f6bef833c3
James <johndoe@example.com>
2018-01-26 17:58:01 -0500

    server update

--- server.rb
+++ server.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
+
 get '/' do
-  "Hello World"
+ File.open("public/hello.txt", "r").read
 end
+
+
 
 get '/sinatra' do
   "Hello World"
 end
+
+#own website
+#reating app... done, ⬢ calm-fortress-51522
+#https://calm-fortress-51522.herokuapp.com/
+#| https://git.heroku.com/calm-fortress-51522.git
